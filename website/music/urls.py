1e64d8184978b0cfd33ec21e1e5e797c5c074310
suyash0103 <suyashghuge@gmail.com>
2018-02-18 00:41:03 +0530

    Static files, Base template, Generic views

--- website/music/urls.py
+++ website/music/urls.py
@@ -3,10 +3,18 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+    # url(r'^$', views.index, name='index'),
 
     # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album_id>/favourite/
+    # url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,26 +1,56 @@
-from django.http import Http404
-# from django.http import HttpResponse
-# from django.template import loader
-from django.shortcuts import render
+# # from django.http import Http404
+# # from django.http import HttpResponse
+# # from django.template import loader
+# from django.shortcuts import render, get_object_or_404
+# from .models import Album, Song
+#
+# def index(request):
+#     all_albums = Album.objects.all()
+#     # html = ''
+#     # for album in all_albums:
+#     #     url = '/music/' + str(album.id) + '/'
+#     #     html += '<a href = "' + url + '">' + album.album_title + '</a><br>'
+#     # template = loader.get_template('music/index.html')
+#     context = {
+#         'all_albums' : all_albums,
+#     }
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'music/index.html', context)
+#
+# def detail(request, album_id):
+#     # return HttpResponse("<h2> Details for album id: " + str(album_id) + "</h2>")
+#     # try:
+#     #     album = Album.objects.get(id=album_id)
+#     # except Album.DoesNotExist:
+#     #     raise Http404("Album does not exist")
+#     album = get_object_or_404(Album, id=album_id)    # Works the same as above try-except statement
+#     return render(request, 'music/detail.html', {'album' : album})
+#     #My app
+#
+# def favourite(request, album_id):
+#     album = get_object_or_404(Album, id=album_id)
+#     try:
+#         selected_song = album.song_set.get(id=request.POST['song']) # Here 'song' is the name attribute of <input> and its value attribute is request.POST['song']
+#     except (KeyError, Song.DoesNotExist):
+#         return render(request, 'music/detail.html', {
+#             'album' : album,
+#             'error_message' : "You did not select a valid song"
+#         })
+#     else:
+#         selected_song.is_favourite = True
+#         selected_song.save()
+#         return render(request, 'music/detail.html', {'album': album})
+
+from django.views import generic
 from .models import Album
 
-def index(request):
-    all_albums = Album.objects.all()
-    # html = ''
-    # for album in all_albums:
-    #     url = '/music/' + str(album.id) + '/'
-    #     html += '<a href = "' + url + '">' + album.album_title + '</a><br>'
-    # template = loader.get_template('music/index.html')
-    context = {
-        'all_albums' : all_albums,
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'music/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def detail(request, album_id):
-    # return HttpResponse("<h2> Details for album id: " + str(album_id) + "</h2>")
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-    return render(request, 'music/detail.html', {'album' : album})
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
